46c074b9f576589a3bf403032c25c8a47231affe
Erik Brown <erik@altschool.com>
2021-02-28 11:42:10 -0800

    adds book ids to local storage

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { useForm } from 'react-hook-form';
 
 import './App.css';
 
-const HTTP_404_NOT_FOUND = 404
+const HTTP_200_OK = 200
+
 
 function SearchBar(props) {
   return (
@@ -12,15 +13,17 @@ function SearchBar(props) {
       <input className="search-bar--Text" />
       <div className="search-bar--Button">Search</div>
     </div>
-  )
+  );
 }
+
 
 function BookList(props) {
   if (!props.books.length) {
     return (
         <div className="book-list-none">
             You do not have any books stored. Please add a book below.
-        </div>)
+        </div>
+    );
   }
 
   return (
@@ -30,17 +33,17 @@ function BookList(props) {
         props.books.map((book, index) => <Book info={book} key={index}/>)
       }
     </div>
-  )
+  );
 }
+
 
 function BookForm(props) {
   const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = data => console.log(data);
 
   return (
     <div className='book-form'>
       <span>Add a book to the list</span>
-      <form onSubmit={handleSubmit(props.onSubmit)}>
+      <form onSubmit={handleSubmit(props.addBook)}>
         <input
           name="isbn"
           placeholder="Enter book ISBN..."
@@ -51,6 +54,7 @@ function BookForm(props) {
     </div>
   );
 }
+
 
 function Book(props) {
   return (
@@ -66,38 +70,74 @@ class App extends Component {
   localStorageKey = 'bookIds'
 
   constructor() {
-    super()
+    super();
     this.state = {
         books: [],
-        bookIds: [],
-    }
+    };
   }
 
   addBook({isbn}) {
+    const cleanISBN = isbn.trim().replaceAll('-', '')
+
+    if (!cleanISBN) {
+        return;
+    }
+
+    return this.fetchBook(cleanISBN).then(book => {
+      if (!book) {
+        return;
+      }
+
+      const books = this.state.books.slice();
+      books.push(book);
+
+      this.setState({books}, this.storeBookId.call(this, cleanISBN));
+    });
+  }
+
+  fetchBook(isbn) {
     const bookKey = `ISBN:${isbn}`;
     const booksUrl = `https://openlibrary.org/api/books?bibkeys=${bookKey}&format=json`;
     return axios.get(booksUrl).then(response => {
-        if (response.status === HTTP_404_NOT_FOUND) {
-            console.log('do something');
-        }
-        const book = response.data[bookKey];
-        const books = this.state.books.slice()
-        const bookIds = this.state.bookIds.slice()
+      if (response.status !== HTTP_200_OK) {
+        return null;
+      }
 
-        books.push(book)
-        bookIds.push(isbn)
-        this.setState({books, bookIds})
-    })
+      return response.data[bookKey];
+    });
   }
 
-  componentDidMount() {}
+  getBookIds() {
+    const storedBookIds = localStorage.getItem(this.localStorageKey);
+
+    if (storedBookIds && storedBookIds.length) {
+        return JSON.parse(storedBookIds);
+    }
+
+    return [];
+  }
+
+  storeBookId(bookId) {
+    const storedBookIds = this.getBookIds();
+    storedBookIds.push(bookId);
+    localStorage.setItem(this.localStorageKey, JSON.stringify(storedBookIds));
+  }
+
+  componentDidMount() {
+    const bookIds = this.getBookIds();
+    return Promise.all(
+        bookIds.map(bookId => this.fetchBook(bookId))
+    ).then(books => {
+        this.setState({books});
+    });
+  }
 
   render() {
     return (
       <div className="App">
         <SearchBar />
         <BookList books={this.state.books}/>
-        <BookForm onSubmit={this.addBook.bind(this)}/>
+        <BookForm addBook={this.addBook.bind(this)}/>
       </div>
     )
   }
